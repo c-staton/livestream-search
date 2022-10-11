ce5b5f1f74d7223451b92987b3bcda529702a787
Chris Staton <topherstaton@gmail.com>
2022-10-11 13:00:12 -0700

    fixed youtube embed for highlight

--- src/top-streamers/HighlightStream.js
+++ src/top-streamers/HighlightStream.js
@@ -3,16 +3,29 @@ import "./styles/HighlightStream.css";
 import StreamInfo from "../common/StreamInfo";
 
 const HighlightStream = ({ stream }) => {
+	let streamEmbed;
+	if (stream.platform === "youtube") {
+		streamEmbed = (
+			<iframe
+				title={stream.title}
+				src={`https://www.youtube.com/embed/${stream.videoId}?autoplay=1&mute=1&enablejsapi=1`}
+			></iframe>
+		);
+	} else {
+		streamEmbed = (
+			<iframe
+				title={stream.title}
+				src={`https://player.twitch.tv/?channel=${stream.channelId}&parent=localhost`}
+			></iframe>
+		);
+	}
 	return (
 		<div className="highlight">
 			<h1 className="highlight__title">
 				Top Streamers <span id="live">Live</span> Right Now
 			</h1>
 			<div className="highlight__embed">
-				<iframe
-					title={stream.title}
-					src={`https://player.twitch.tv/?channel=${stream.channelId}&parent=localhost`}
-				></iframe>
+				{streamEmbed}
 				<StreamInfo
 					title={stream.title}
 					platform={stream.platform}
